1fcf890570b592ad6db0b3b9e25593721ce524a3
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-07-27 11:00:17 +0200

    Add files via upload

---
+++ run_plt.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import pandas as pd
+import matplotlib
+import matplotlib.pylab as plt
+
+class Doc:
+    """read the out file of the thermo output and plot all of the data
+    Input:
+        sys.argv[1] -> thermo output
+    Output:
+        png figs
+    """
+
+class ReadData:
+    """read the data file and return the DataFrame
+    if any modification is needed should be done here"""
+    
