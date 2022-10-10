845003017745a64925ad18e1858a966ab17d7c41
Saeed Amiri <55943577+saeed-amiri@users.noreply.github.com>
2022-10-10 08:53:40 +0200

    Update speed_up.py

--- speed_up.py
+++ speed_up.py
@@ -96,7 +96,7 @@ class PlotDf:
         inches_per_pt = 1/72.27
         golden_ratio = (5**0.5 - 1)/2
         fig_width_in = fig_width_pt * inches_per_pt
-        fig_height_in = fig_width_in * golden_ratio*1
+        fig_height_in = fig_width_in * golden_ratio*0.73
         fig_dim = (fig_width_in, fig_height_in)
         return fig_dim
 
